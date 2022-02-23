3a15b309e87077a8ed4dd4ff0e4b273f8222750a
AphiweMp <aphiwempeto@gmail.com>
2022-02-23 16:30:32 +0200

    first commit

---
+++ controllers/main.js
@@ -0,0 +1,24 @@
+ const req = require("express/lib/request")
+
+const CustomAPIError = require('../errors/custom-error')
+
+const login = async (req, res) => {
+    const {username, password} = req.body
+
+    if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400)
+    }
+    
+    console.log(username, password);
+    res.send('Fake Login/Register/Signup Route')
+    
+}
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100)
+  res.status(200).json({message:`Hello, Aphiwe`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+}
+module.exports = {
+ login, dashboard
+}
+
